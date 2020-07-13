9200b1f880daf0a9f8765b3fc5d664e279dca482
JonBuluc <jonanb515@gmail.com>
2020-07-12 23:19:50 -0500

    Se elimino la division sobre 0 para conseguir ceros debajo de la diagonal principal faltaria comprobar si es necesario evitar la division sobre 0 cuando se obtienen ceros arriba de la diagonal principal, el metodo usado para evitar la disivion sobre 0 fue la operacion elemental cambio de renglon seleccionando como pivote el que tuviera el mayor valor absoluto con esto se reduce el error al trabajar con numeros racionales

--- Gauss-Jordan.py
+++ Gauss-Jordan.py
@@ -34,15 +34,40 @@ print()
 #obtener el renglon pivote
 #el proceso de Gauss Jordan es por los renglones el for nos servira para hacer las respectivas operaciones
 for i in range(renglones):
-	#este for sera para que el elemento i de la columna se divida entre los demas y asi mismo al final
-	#se guardara el elemento en un espacio para que cuando se divida entre si misma no se pierda su  valor
-	#orginal y asi no afecte los elementos que van despues de el
-	memoria=matriz[i][i]
-	for j in range(columnas):
-		matriz[i][j]=matriz[i][j]/memoria
 	#condicion especial de una accion que se hara si el renglon que se esta trabajando no es el ultimo
 	#ya que si es el ultimo no tiene que restar sus elementos a un renglon inferior porque no existe un renglon inferior
 	if i!=renglones-1:
+		#declaracion de una variable para que el renglon pivote sea el que tenga el mayor valor absoluto evitando asi que
+		#que el elemento sea 0 y asi no habra division sobre 0 ademas si se elige el de mayor valor absoluto hace mas pequeño
+		#los errores decimales
+		mayor=i
+		#el for hara que se comparen todos los elementos que pueden ser candidatos a ser el pivote al final solo quedara el
+		#del valor absoluto mas grande
+		for ii in range(renglones-i-1):
+			if abs(matriz[mayor][i])<abs(matriz[ii+1][i]):
+				mayor=ii+1
+		#si el renglon pivote seleccionado es en el que ya se estaba trabajando no se tendra que hacer nada pero si es otro
+		#se tendra que hacer la operacion elemental de cambio de renglon
+		if mayor!=i:
+			#se guardara el renglon a quitar en esta memoria
+			memoriarenglon=[]
+			#el for sera para que se haga el cambio elemento a elemento recorriendo el renglon
+			for j in range (columnas):
+				#primero se guarda el renglon que se va a reemplazar
+				memoriarenglon.append(float(matriz[i][j]))
+				#luego se intercambia el renglon por el nuevo renglon pivote
+				matriz[i][j]=matriz[mayor][j]
+				#despues se regresa el antiguo renglon pivote al lugar donde estaba el nuevo renglon pivote
+				matriz[mayor][j]=memoriarenglon[j]
+		#si todos los elementos llegaran a ser 0 se pondra un if para pasar a la siguiente iteracion del i principal
+		if matriz[i][i]==0:
+			continue
+		#este for sera para que el elemento i de la columna se divida entre los demas y asi mismo al final
+		#se guardara el elemento en un espacio para que cuando se divida entre si misma no se pierda su  valor
+		#orginal y asi no afecte los elementos que van despues de el
+		memoria=matriz[i][i]
+		for j in range(columnas):
+			matriz[i][j]=matriz[i][j]/memoria
 		#este for es para que recorra los renglones inferiores asi si se encuentra en el renglon 0 de 2 
 		#hara la accion para el renglon 1 y 2 y si se encuentra en el renglon  2 de 3 hara la accion para el renglon 3 
 		for k in range(renglones-i-1):
